9089057f4fc0f53b66b5db3199e79dfe345adbbc
stanleycai95 <46463107+stanleycai95@users.noreply.github.com>
2023-01-29 17:16:36 -0500

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class DecisionTree:
     
-    def __init__(self, X, y, method='C4.5', depth=3):
+    def __init__(self, X, y, method='C4.5', depth=5, attributes=None):
         self.method = method
         self.depth = depth
         self.split_threshold = None
@@ -11,6 +11,10 @@ class DecisionTree:
         
         self.X = X
         self.y = y
+        self.attributes = attributes
+        if self.attributes is None:
+            self.attributes = list(range(self.X.shape[1]))
+            
         self.left = None
         self.right = None
         self.label = None
@@ -38,18 +42,14 @@ class DecisionTree:
         
         return -np.sum(p * np.log2(p))
     
-    def accuracy(self, y_pred, y):
-        return np.mean(y_pred == y)
-    
     def fit(self):
         self.fitted = True
         
         if (self.depth == 0) or (np.mean(self.y) == 0) or (np.mean(self.y) == 1):
             self.label = (np.mean(self.y) > 0.5).astype(int)
         else:
-            attributes = list(range(self.X.shape[1]))
                 
-            for attr in attributes:
+            for attr in self.attributes:
                 sort_order = self.X[:, attr].argsort()
                 self.X = self.X[sort_order]
                 self.y = self.y[sort_order]
@@ -88,6 +88,47 @@ class DecisionTree:
             
             return ans
 
+class RandomForest:
+    
+    def __init__(self, X, y, method='C4.5', depth=5, num_trees=10):
+        self.method = method
+        self.depth = depth
+        self.X = X
+        self.y = y
+        self.num_trees = num_trees
+        self.decision_trees = []
+        
+    def fit(self):
+        num_features_per_tree = np.around(np.sqrt(X.shape[1])).astype(int)
+        potential_features = np.array(list(range(self.X.shape[1])))
+        num_samples_per_tree = X.shape[0] * 2 // 3
+        potential_samples = np.array(list(range(self.X.shape[0])))
+        
+        for i in range(self.num_trees):
+            feature_subset = np.random.choice(potential_features, num_features_per_tree, replace=False)
+            row_subset = np.random.choice(potential_samples, num_samples_per_tree, replace=False)
+            
+            dt = DecisionTree(X=X[row_subset,:], y=y[row_subset], attributes=feature_subset)
+            dt.fit()
+            self.decision_trees.append(dt)
+
+    def predict(self, X):
+        assert len(self.decision_trees) > 0, "Random Forest must be fit before prediction"
+        
+        ans = np.zeros(X.shape[0])
+        for dt in self.decision_trees:
+            ans += dt.predict(X)
+        
+        ans /= len(self.decision_trees)
+        
+        ans[ans<=0.5] = 0
+        ans[ans>0.5] = 1
+        
+        return ans
+            
+def accuracy(y_pred, y):
+    return np.mean(y_pred == y)
+
 from sklearn.datasets import load_breast_cancer
 
 X, y = load_breast_cancer(return_X_y=True)
@@ -102,6 +143,15 @@ dt = DecisionTree(X_train, y_train)
 dt.fit()
 y_pred = dt.predict(X_test)
 
-print(np.vstack((y_pred, y_test)).T)
-print(dt.accuracy(y_pred, y_test))
+rf = RandomForest(X_train, y_train)
+rf.fit()
+y_pred_rf = rf.predict(X_test)
+
+print("Decision tree accuracy")
+print(accuracy(y_pred, y_test))
+
+print("Random Forest accuracy")
+print(accuracy(y_pred_rf, y_test))
+
+print("Class balance check")
 print(np.mean(y))
